ce64315b7ec7271a61f21175d8f8cb011b244439
Fatalis <fatalis.twitch@gmail.com>
2015-05-21 18:58:35 -0500

    add setting, use hooks, and WIP durability code

--- dllmain.d
+++ dllmain.d
@@ -10,7 +10,12 @@ import std.conv: to;
 import wrapper: DirectInput8Create_Real;
 import windows;
 
-__gshared HINSTANCE g_hInst;
+const int JMP_SIZE = 12;
+__gshared {
+    void* g_hInst;
+    float g_weightMultiplier = 1;
+    float g_durabilityMultiplier = 1;
+}
 
 extern (Windows)
 bool DllMain(void* hInstance, uint ulReason, void* pvReserved)
@@ -21,6 +26,7 @@ bool DllMain(void* hInstance, uint ulReason, void* pvReserved)
         g_hInst = hInstance;
         dll_process_attach(hInstance, true);
         InitWrapper();
+        LoadSettings();
         PatchCode();
         break;
 
@@ -52,22 +58,123 @@ void InitWrapper()
     DirectInput8Create_Real = cast(typeof(DirectInput8Create_Real))GetProcAddress(dinput, "DirectInput8Create");
 }
 
+void LoadSettings()
+{
+    char ini[MAX_PATH];
+    GetCurrentDirectoryA(ini.sizeof, ini.ptr);
+    strcat(ini.ptr, r"\witcher3weight.ini");
+    DebugPrint("ini %s", ini);
+
+    char weight[32];
+    GetPrivateProfileStringA("Settings", "ItemWeightMultiplier", "1.0", weight.ptr, weight.sizeof, ini.ptr);
+    debug {
+    char durability[32];
+    GetPrivateProfileStringA("Settings", "ItemMaxDurabilityMultiplier", "1.0", durability.ptr, durability.sizeof, ini.ptr);
+    }
+
+    try
+    {
+        g_weightMultiplier = to!float(to!string(weight.ptr));
+        debug g_durabilityMultiplier = to!float(to!string(durability.ptr));
+    }
+    catch (Exception ex)
+    {
+        DebugPrint(ex.toString());
+    }
+
+    DebugPrint("settings %f %f", g_weightMultiplier, g_durabilityMultiplier);
+
+}
+
 void PatchCode()
 {
-    auto func = GetScriptFunc("GetItemWeight");
-    if (!func)
+    auto weightFunc = GetScriptFunc("GetItemWeight");
+    if (!weightFunc)
         return;
 
-    // .text:00007FF6B4245A6C F3 0F 11 06    movss   dword ptr [rsi], xmm0
-    byte[] instr = [cast(byte)0xf3, cast(byte)0x0f, cast(byte)0x11, cast(byte)0x06]; // figure out why these nasty casts are needed
-    auto instrAddr = FindBytes(instr.ptr, instr.length, cast(byte*)func, 128);
-    if (!instrAddr)
+    InstallTrampoline(cast(ubyte*)weightFunc, cast(ubyte*)&GetItemWeight_Hook, cast(ubyte*)&GetItemWeight_Gate, JMP_SIZE+3);
+
+    debug {
+    auto maxDuraFunc = GetScriptFunc("GetItemMaxDurability");
+    if (!maxDuraFunc)
         return;
-    
-    DebugPrint("instrAddr %X", instrAddr);
-    // xor eax ,eax
-    // mov dword ptr [rsi], eax
-    memcpy_protected(instrAddr, "\x31\xc0\x89".ptr, 3);
+
+    InstallTrampoline(cast(ubyte*)maxDuraFunc, cast(ubyte*)&GetItemMaxDurability_Hook, cast(ubyte*)&GetItemMaxDurability_Gate, JMP_SIZE+3);
+
+    auto initialDuraFunc = GetScriptFunc("GetItemInitialDurability");
+    if (!initialDuraFunc)
+        return;
+
+    InstallTrampoline(cast(ubyte*)initialDuraFunc, cast(ubyte*)&GetItemInitialDurability_Hook, cast(ubyte*)&GetItemInitialDurability_Gate, JMP_SIZE+3);
+    }
+}
+
+extern(C++)
+void GetItemWeight_Hook(void* a1, void* a2, float* weight)
+{
+    GetItemWeight_Gate(a1, a2, weight);
+    if (weight !is null)
+    {
+        DebugPrint("weight %f", *weight);
+        *weight /= g_weightMultiplier;
+    }
+}
+
+extern(C++)
+void GetItemWeight_Gate(void* a1, void* a2, float* weight)
+{
+	asm {
+		naked;
+        nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;
+        nop; nop; nop;
+        nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;
+	}
+}
+
+debug {
+extern(C++)
+void GetItemMaxDurability_Hook(void* a1, void* a2, float* durability)
+{
+    GetItemMaxDurability_Gate(a1, a2, durability);
+    if (durability !is null)
+    {
+        DebugPrint("durability %f", *durability);
+        *durability *= g_durabilityMultiplier;
+    }
+}
+
+extern(C++)
+void GetItemMaxDurability_Gate(void* a1, void* a2, float* durability)
+{
+	asm {
+		naked;
+        nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;
+        nop; nop; nop;
+        nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;
+	}
+}
+
+extern(C++)
+void GetItemInitialDurability_Hook(void* a1, void* a2, float* durability)
+{
+    GetItemInitialDurability_Gate(a1, a2, durability);
+    if (durability !is null)
+    {
+        DebugPrint("initial durability %f", *durability);
+        *durability *= g_durabilityMultiplier;
+    }
+}
+
+extern(C++)
+void GetItemInitialDurability_Gate(void* a1, void* a2, float* durability)
+{
+	asm {
+		naked;
+        nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;
+        nop; nop; nop;
+        nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;
+	}
+}
 }
 
 void* GetScriptFunc(const wchar[] name)
@@ -78,10 +185,10 @@ void* GetScriptFunc(const wchar[] name)
     if (!GetModuleInformation(GetCurrentProcess(), mod, &info, info.sizeof))
         return null;
 
-    auto base = cast(byte*)info.lpBaseOfDll;
+    auto base = cast(ubyte*)info.lpBaseOfDll;
     auto size = info.SizeOfImage;
 
-    auto nameAddr = FindBytes(cast(byte*)name.ptr, name.length*wchar.sizeof, base, size);
+    auto nameAddr = FindBytes(cast(ubyte*)name.ptr, name.length*wchar.sizeof, base, size);
     if (!nameAddr)
         return null;
 
@@ -90,8 +197,8 @@ void* GetScriptFunc(const wchar[] name)
         return null;
 
     // .text:00007FF6B424ED73 48 8D 05 96 6C FF FF    lea rax, GetItemWeight
-    byte[] lea2 = [cast(byte)0x48, cast(byte)0x8D, cast(byte)0x05];
-    auto lea2Addr = FindBytesReverse(lea2.ptr, lea2.length, cast(byte*)leaAddr, 64);
+    ubyte[] lea2 = [0x48, 0x8D, 0x05];
+    auto lea2Addr = FindBytesReverse(lea2.ptr, lea2.length, cast(ubyte*)leaAddr, 64);
     if (!lea2Addr)
         return null;
 
@@ -102,20 +209,22 @@ void* GetScriptFunc(const wchar[] name)
 }
 
 // .text:00007FF6B424ED9A 48 8D 15 A7 B3 71 01    lea rdx, aGetitemweight 
-void* FindLEARDXForAddr(void* addr, byte* mem, size_t size)
+void* FindLEARDXForAddr(void* addr, ubyte* mem, size_t size)
 {
-    byte[] lea = [cast(byte)0x48, cast(byte)0x8D, cast(byte)0x15];
-    foreach (byte* i; FindAllBytes(lea, mem, size))
+    ubyte[] lea = [0x48, 0x8D, 0x15];
+    foreach (ubyte* i; FindAllBytes(lea, mem, size))
     {
         auto encodedAddr = cast(int)(addr - (i + 7));
         if (*cast(int*)(i+3) == encodedAddr)
+        {
             return i;
+        }
     }
 
     return null;
 }
 
-void* FindBytes(byte* search, size_t size, byte* mem, size_t len)
+void* FindBytes(ubyte* search, size_t size, ubyte* mem, size_t len)
 {
     for (auto ptr = mem; ptr < mem + len - size; ptr++)
     {
@@ -128,7 +237,7 @@ void* FindBytes(byte* search, size_t size, byte* mem, size_t len)
     return null;
 }
 
-void* FindBytesReverse(byte* search, size_t size, byte* mem, size_t len)
+void* FindBytesReverse(ubyte* search, size_t size, ubyte* mem, size_t len)
 {
     for (auto ptr = mem-size; ptr >= mem-len; ptr--)
     {
@@ -141,9 +250,9 @@ void* FindBytesReverse(byte* search, size_t size, byte* mem, size_t len)
     return null;
 }
 
-byte*[] FindAllBytes(const byte[] search, byte* mem, size_t len)
+ubyte*[] FindAllBytes(const ubyte[] search, ubyte* mem, size_t len)
 {
-    byte*[] ret;
+    ubyte*[] ret;
     
     for (auto ptr = mem; ptr < mem + len - search.length; ptr++)
     {
@@ -162,6 +271,38 @@ void DebugPrint(Char, Args...)(Char[] fmt, Args args)
         string str = format(fmt, args);
         OutputDebugStringA(str.toStringz());
     }
+}
+
+void WriteJMP(ubyte* src, ubyte* dest, int nops)
+{
+	uint oldProtect;
+	if (VirtualProtect(src, JMP_SIZE+nops, PAGE_EXECUTE_READWRITE, &oldProtect))
+	{
+        *(src) = 0x48; // mov rax immediate
+        *(src+1) = 0xb8;
+        *cast(void**)(src+2) = dest;
+        *(src+10) = 0xff; // jmp rax
+        *(src+11) = 0xe0;
+
+		for (int i = 0; i < nops; i++)
+			*(src + JMP_SIZE + i) = 0x90;
+
+		VirtualProtect(src, JMP_SIZE+nops, oldProtect, &oldProtect);
+	}
+}
+
+void InstallTrampoline(ubyte* src, ubyte* dest, ubyte* gate, int overwritten)
+{
+	uint oldProtect;
+	if (VirtualProtect(gate, overwritten, PAGE_EXECUTE_READWRITE, &oldProtect))
+	{
+		memcpy(gate, src, overwritten);
+		//gate[0..overwritten-1] = src[0..overwritten-1];
+		VirtualProtect(gate, overwritten, oldProtect, &oldProtect);
+	}
+
+	WriteJMP(gate+overwritten, src+overwritten, 0);
+	WriteJMP(src, dest, (overwritten > JMP_SIZE ? overwritten - JMP_SIZE : 0));
 }
 
 bool memcpy_protected(void* dest, in void* src, size_t size)
--- windows.d
+++ windows.d
@@ -4,6 +4,7 @@ extern(Windows) {
     void OutputDebugStringA(const char* lpPathName);
     bool GetModuleInformation(void* hProcess, void* hModule, MODULEINFO* lpmodinfo, uint cb);
     //uint GetSystemDirectoryA(const char* lpBuffer, uint uSize);
+    uint GetPrivateProfileStringA(const char* lpAppName, const char* lpKeyName, const char* lpDefault, char* lpReturnedString, uint nSize, const char* lpFileName);
 }
 
 struct MODULEINFO { 
