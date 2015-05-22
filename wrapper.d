d70933cd6dbffc29e61e20c6dc5d63b40162c29d
Fatalis <fatalis.twitch@gmail.com>
2015-05-22 17:44:05 -0500

    steam overlay compatibility

--- wrapper.d
+++ wrapper.d
@@ -4,10 +4,19 @@ export {
     extern (Windows)
     void* DirectInput8Create(void* hinst, uint dwVersion, void* riidltf, void** ppvOut, void* punkOuter)
     {
-        if (!DirectInput8Create_Real)
-            return null;
-        return DirectInput8Create_Real(hinst, dwVersion, riidltf, ppvOut, punkOuter);
+        asm {
+            naked;
+            jmp DirectInput8Create_Hook; // steam overlay compatibility
+        }
     }
+}
+
+extern (Windows)
+void* DirectInput8Create_Hook(void* hinst, uint dwVersion, void* riidltf, void** ppvOut, void* punkOuter)
+{
+    if (!DirectInput8Create_Real)
+        return null;
+    return DirectInput8Create_Real(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 }
 
 __gshared {
